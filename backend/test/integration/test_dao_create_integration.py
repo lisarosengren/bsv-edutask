cface40a7dc79a7c05343437a50e94276ba1d32f
Lisa Rosengren <lisaalexandrarosengren@gmail.com>
2026-04-17 14:12:27 +0200

    removed unit directory to avoid merge conflicts. updated test file

--- backend/test/integration/test_dao_create_integration.py
+++ backend/test/integration/test_dao_create_integration.py
@@ -2,9 +2,9 @@ from src.util.dao import DAO
 import pytest
 from unittest.mock import patch
 import pymongo
+# import os
 
-
-# ändrat i .env = MONGO_URL=mongodb://root:root@localhost:27017
+# os.environ['MONGO_URL'] = "mongodb://root:root@localhost:27017"
 
 
 class TestDaoToMongo():
@@ -62,13 +62,30 @@ class TestDaoToMongo():
             result = dao.create({"name": "rebecka", "city": "Malmö", "stupid": False})
 
     @pytest.mark.integration
-    def test_dao_to_mongo_valid_input(self, dao):
+    def test_dao_to_mongo_valid_input_correct_length(self, dao):
         
         result = dao.create({"name": "lisa", "city": "Malmö", "stupid": True})
 
         assert len(result) == 4
-        assert result["name"] == "lisa"
-        assert result["city"] == "Malmö"
-        assert result["stupid"] == True
+
+    @pytest.mark.parametrize("key, value", [
+        ("name", "lisa"),
+        ("city", "Malmö"),
+        ("stupid", True)
+        ])
+    @pytest.mark.integration
+    def test_dao_to_mongo_valid_input_correct_content(self, dao, key, value):
+        
+        result = dao.create({"name": "lisa", "city": "Malmö", "stupid": True})
+
+        assert result[key] == value
+
+
+    @pytest.mark.integration
+    def test_dao_to_mongo_valid_input_correct_id_is_added(self, dao):
+        
+        result = dao.create({"name": "lisa", "city": "Malmö", "stupid": True})
+
         assert result["_id"] is not None
+
         
